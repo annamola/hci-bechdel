a488fcb0127877a3600e722f1929e09447d12dd1
annamola <annamola12345@gmail.com>
2022-02-26 19:02:46 -0500

    added paragraph

--- src/Persuasion/Persuasion.js
+++ src/Persuasion/Persuasion.js
@@ -7,15 +7,20 @@ import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
 
 function Persuasion() {
     const data = require("../assets/data/nextBechdel_allTests.json");
+
+    const passed_color = "green";
+    const failed_color = "red";
+
     function handleColor(e) {
         if (e === 1) {
             //failed red
-            return "red";
+            return failed_color;
         } else {
             //passed green
-            return "green";
+            return passed_color;
         }
     }
+
     const LightTooltip = styled(({ className, ...props }) => <Tooltip {...props} classes={{ popper: className }} />)(
         ({ theme }) => ({
             [`& .${tooltipClasses.tooltip}`]: {
@@ -36,23 +41,34 @@ function Persuasion() {
             <div>
                 <div>
                     <h1 className="title">Looking Beyond the Bechdel Test</h1>
+                    <p className="para">
+                        These days almost everyone knows about the Bechdel Test. It asks the two questions:
+                    </p>
+
+                    <ul className="para">
+                        <li>Does it have at least two named female characters?</li>
+                        <li>And do those characters have at least one conversation that is not about a man?</li>
+                    </ul>
+                    <p className="para">
+                        It's shocking how many movies still do not pass this test. Every year representation improves on
+                        screen yet there are many gaps in diversity and representation behind the scenes. <br />
+                        We're going to take a deep dive in to the most popular movies of 2016 and whether they hold up
+                        to the Bechdel test. We will also examine some lesser known tests that look at other aspects of
+                        diversity and represtation that can be overlooked when just using the Bechdel Test.
+                    </p>
                     <div className="legend">
                         <div className="legend_section">
-                            <div className="legend_box" style={{ backgroundColor: "red" }} />
+                            <div className="legend_box" style={{ backgroundColor: failed_color }} />
                             <p>Failed</p>
                         </div>
                         <div className="legend_section">
-                            <div className="legend_box" style={{ backgroundColor: "green" }} />
+                            <div className="legend_box" style={{ backgroundColor: passed_color }} />
                             <p>Passed</p>
                         </div>
                     </div>
                 </div>
                 <div className="vis_container">
                     <p className="sub_title">Bechdel Test</p>
-                    <p>
-                        Does it have at least two named female characters?<br></br>
-                        And do those characters have at least one conversation that is not about a man?
-                    </p>
                     <div className="vis">
                         {data.map((movie) => (
                             <LightTooltip key={movie.movie} placement="bottom" title={movie.movie}>

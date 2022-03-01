a46a545a53335d27705477aa0f376bb797a23a35
annamola <annamola12345@gmail.com>
2022-02-28 20:25:23 -0500

    fix

--- src/Communication/Communication.js
+++ src/Communication/Communication.js
@@ -1,7 +1,8 @@
 import Chart from "../Chart/Chart";
 import chartSpec1 from "./chartspec1.json";
 import chartSpec2 from "./chartspec2.json";
-import chartSpec3 from "./chartspec3.json";
+import sing_cast from "./sing_cast.json";
+import crew from "./crew.json";
 
 function Communication() {
     return (
@@ -14,7 +15,16 @@ function Communication() {
                         <div className="home__data">
                             <h1 className="home__title">Communication</h1>
                         </div>
-                        <p>"The Bechdel test was created in 1985 as a metric to determine the portrayal of women in film.  To pass the Bechdel test, a film must have two named female characters who have at least one conversation that is not primarily about a man.  As the movie industry becomes increasingly more representative, tests with more specific criteria have been created.  These tests can generally be categorized into four categories: “protagonist”, that analyzes presence of stereotypes in female protagonists; “supporting cast”, that looks at the gender distribution of smaller roles; “intersectionality”, that examines the amount of non-white women in film; and “crew”, that looks at the number of women behind the camera".</p>
+                        <p>
+                            "The Bechdel test was created in 1985 as a metric to determine the portrayal of women in
+                            film. To pass the Bechdel test, a film must have two named female characters who have at
+                            least one conversation that is not primarily about a man. As the movie industry becomes
+                            increasingly more representative, tests with more specific criteria have been created. These
+                            tests can generally be categorized into four categories: “protagonist”, that analyzes
+                            presence of stereotypes in female protagonists; “supporting cast”, that looks at the gender
+                            distribution of smaller roles; “intersectionality”, that examines the amount of non-white
+                            women in film; and “crew”, that looks at the number of women behind the camera".
+                        </p>
                         <div className="legend">
                             <div className="legend_section">
                                 <div className="legend_box" style={{ backgroundColor: "#5ec962" }} />
@@ -36,7 +46,7 @@ function Communication() {
                             width={700}
                             chartTitle="Cast and Crew Gender Distribution for Finding Dory (2016)"
                         />
-                                <Chart
+                        <Chart
                             spec={crew}
                             width={700}
                             chartTitle="Cast and Crew Gender Distribution for Finding Dory (2016)"
